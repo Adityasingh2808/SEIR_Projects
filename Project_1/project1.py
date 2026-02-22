8276a4a7a016fc93cb20a3fce2173826c9c8df03
Aditya <aadik7212@gmail.com>
2026-02-22 22:25:41 +0530

    Add rolling_hash, simhash, cal_hamming function

--- Project_1/project1.py
+++ Project_1/project1.py
@@ -50,7 +50,21 @@ def extract_links(html, base_url):
     return all_links
 
 def get_words_list(text):
-    words = re.findall(r"[a-zA-Z0-9]+", text.lower())
+    text = text.lower()
+    words = []
+    current_word = ""
+
+    for char in text:
+        if char.isalnum():
+            current_word += char
+        else:
+            if current_word != "":
+                words.append(current_word)
+                current_word = ""
+                
+    if current_word != "":
+        words.append(current_word)
+
     return words
 
 def cal_frequency(text):
@@ -64,5 +78,43 @@ def cal_frequency(text):
             frequency[word] = 1
     return frequency
 
+def rolling_hash(word):
+    base = 53
+    mod = 2**64
+    hash_value = 0
 
+    for char in word:
+        hash_value = (hash_value * base + ord(char)) % mod
 
+    return hash_value
+
+def simhash(text):
+    freq = cal_frequency(text)
+    bit_vector = [0] * 64 
+
+    for word in freq:
+        h = rolling_hash(word)
+        for i in range(64):
+            bit = (h >> i) & 1
+            if bit == 1:
+                bit_vector[i] += freq[word]
+            else:
+                bit_vector[i] -= freq[word]
+
+    final_hash = 0
+    for i in range(64):
+        if bit_vector[i] > 0:
+            final_hash |= (1 << i)
+
+    return final_hash
+
+def cal_hamming(hash1, hash2):
+    xor_value = hash1 ^ hash2
+    count = 0
+
+    while xor_value > 0:
+        if xor_value % 2 == 1:
+            count += 1
+        xor_value = xor_value // 2
+
+    return count
